1574a9651f400a067981c5f8e19991cad3abee8e
castanedaLuis <castanedaLuis@github.com>
2023-01-23 23:24:35 -0600

    Creando el provedor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { SessionProvider as AuthProvider} from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
